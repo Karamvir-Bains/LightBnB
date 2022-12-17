8142d9f7e887809aaed98294d41e87366e9512b5
Karamvir-Bains <kbains2000@gmail.com>
2022-12-16 20:20:05 -0700

    Update Suggested Project Structure

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,38 +1,16 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
-});
-
-pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {
-  console.log(response);
-});
-
 /// Users
-
+const db = require("./db");
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  return pool
-    .query(`SELECT * FROM users WHERE email = $1`, [email])
-    .then((result) => {
-      let user = result.rows[0];
-      if (!user) {
-        user = null;
-      }
-      return user;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  let queryParams = [email];
+  let queryString = `SELECT * FROM users WHERE email = $1`;
+  return db.query(queryString, queryParams)
+    .then((result) => result.rows ? result.rows[0] : null)
+    .catch((err) => console.log(err.message));
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -43,18 +21,11 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return pool
-    .query(`SELECT * FROM users WHERE id = $1`, [id])
-    .then((result) => {
-      let user = result.rows[0];
-      if (!user) {
-        user = null;
-      }
-      return user;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  let queryParams = [id];
+  let queryString = `SELECT * FROM users WHERE id = $1`;
+  return db.query(queryString, queryParams)
+    .then((result) => result.rows ? result.rows[0] : null)
+    .catch((err) => console.log(err.message));
 };
 exports.getUserWithId = getUserWithId;
 
@@ -65,18 +36,14 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  return pool
-    .query(`INSERT INTO users(name, email, password)
-    VALUES($1, $2, $3)
-    RETURNING *;`,
-    [user.name, user.email, user.password])
-    .then((result) => {
-      let user = result.rows[0];
-      return user;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  let queryParams = [user.name, user.email, user.password];
+  let queryString = `
+  INSERT INTO users(name, email, password)
+  VALUES($1, $2, $3)
+  RETURNING *;`;
+  return db.query(queryString, queryParams)
+    .then((result) => result.rows[0])
+    .catch((err) => console.log(err.message));
 };
 exports.addUser = addUser;
 
@@ -88,19 +55,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return pool
-    .query(`SELECT reservations.*, properties.*
-    FROM reservations
-    JOIN properties ON properties.id = reservations.property_id
-    WHERE guest_id = $1
-    LIMIT $2;`,
-    [guest_id, limit])
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  let queryParams = [guest_id, limit];
+  let queryString = `
+  SELECT reservations.*, properties.*
+  FROM reservations
+  JOIN properties ON properties.id = reservations.property_id
+  WHERE guest_id = $1
+  LIMIT $2;`;
+  return db.query(queryString, queryParams)
+    .then((result) => result.rows)
+    .catch((err) => console.log(err.message));
 };
 exports.getAllReservations = getAllReservations;
 
@@ -115,7 +79,6 @@ exports.getAllReservations = getAllReservations;
 
 const getAllProperties = function (options, limit = 10) {
   const queryParams = [];
-
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
@@ -158,9 +121,9 @@ const getAllProperties = function (options, limit = 10) {
   LIMIT $${queryParams.length};
   `;
 
-  console.log(queryString, queryParams);
-
-  return pool.query(queryString, queryParams).then((res) => res.rows);
+  return db.query(queryString, queryParams)
+    .then((res) => res.rows)
+    .catch((err) => console.log(err.message));
 };
 exports.getAllProperties = getAllProperties;
 
@@ -195,13 +158,8 @@ const addProperty = function(property) {
   VALUES(${values})
   RETURNING *;`;
 
-  return pool.query(queryString, queryParams)
-    .then((res) => {
-      console.log('INSERT property:', res.rows[0]);
-      return res.rows[0];
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  return db.query(queryString, queryParams)
+    .then((res) => res.rows[0])
+    .catch((err) => console.log(err.message));
 };
 exports.addProperty = addProperty;
